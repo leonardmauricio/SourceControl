a2205e65c5b5349bafb0aee7746f483f514c8571
Leonard Mauricio <leonard.mauricio@outlook.com>
2024-08-23 14:37:36 +0200

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,32 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Mauricio Leonard on 23.08.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SourceControlApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
